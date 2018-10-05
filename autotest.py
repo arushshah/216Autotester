2a09fff8f19a7dd9fa3adf47db32dfdc38f2d972
Arush Shah <arushah@grace2.umd.edu>
2018-10-05 12:25:20 -0400

    added input file conditional and print statements

--- autotest.py
+++ autotest.py
@@ -2,6 +2,9 @@ import os
 import re
 import sys
 
+if (len(sys.argv) < 2):
+    print("Must enter at least one source file as an argument")
+    sys.exit()
 STUDENT_SCRIPTS = sys.argv[1:]
 source_files = ""
 for file in STUDENT_SCRIPTS:
@@ -14,6 +17,7 @@ INPUT_LIST = []
 OUTPUT_LIST = []
 
 input_ext = False;
+output_ext = False;
 
 for file in FILES:
     raw_match = re.search(r'public[0-9]+[.]c', file)
@@ -25,20 +29,32 @@ for file in FILES:
         INPUT_LIST.append(raw_match.group())
     raw_match = re.search(r'public[0-9]+[.]output', file)
     if (raw_match != None):
+        output_ext = True
         OUTPUT_LIST.append(raw_match.group())
 
 INPUT_LIST.sort()
 OUTPUT_LIST.sort()
+count = 1;
 
 if (not input_ext):
     for file in INPUT_LIST:
         os.system("gcc " + source_files + " " + file + " -o " + file[0:-2] + ".x")
-    for file in INPUT_LIST:
-        os.system(file[0:-2] + ".x")
+    if (not output_ext):
+        for file in INPUT_LIST:
+            print("\nTest " + str(count) + " Running...\n")
+            os.system(file[0:-2] + ".x")
+            count+=1
+    else:
+        for file in OUTPUT_LIST:
+            print("\nTest " + str(count) + " Running...\n")
+            os.system(file[0:-7] + ".x | " + "diff -b - " + file)
+            count+=1
 
 else:
     for file in INPUT_LIST:
         os.system("gcc " + source_files + " -o " + file[0:-6] + ".x")
     for file in INPUT_LIST:
+        print("\nTest " + str(count) + " Running...\n")
         os.system(file[0:-6] + ".x < " + file + " | diff -b - " + file[0:-6] + ".output")
+        count+=1
         
